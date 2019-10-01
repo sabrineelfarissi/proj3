f2878451d0f18297bf19523b4c790b6592b59585
Sabrine Elfarissi <selfarissi101@gmail.com>
2019-09-30 19:37:44 -0700

    Initial Commit

---
+++ proj3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proj3
+//
+//  Created by Sabrine Elfarissi on 9/30/19.
+//  Copyright © 2019 Sabrine Elfarissi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
